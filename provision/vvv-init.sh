21a98b5d1ab9b5edef6e8ddb3945c44d7e06e758
shoreline-chrism <chrism@shorelinemedianj.com>
2019-04-29 10:35:04 -0400

    Adding SSL provisioning
    Added SSL provisioning from VVV starter template provision script

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -190,6 +190,15 @@ npm install -g bower gulp-cli
 # Replace nginx config with location of custom site certificates
 sed -i "s#{vvv_db_name}#${DB_NAME}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 
+cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+
+if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
+    sed -i "s#{{TLS_CERT}}#ssl_certificate /vagrant/certificates/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}#ssl_certificate_key /vagrant/certificates/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+else
+    sed -i "s#{{TLS_CERT}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+fi
 
 # Install Liquidprompt on first provision only
 if [[ ! -d "/home/vagrant/liquidprompt" ]]; then
