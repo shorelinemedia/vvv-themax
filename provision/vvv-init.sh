e3f3240e32d8d65cb61089bd26badc636e5e63be
shoreline-chrism <chrism@shorelinemedianj.com>
2019-04-29 12:16:37 -0400

    Update vvv-init.sh

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -182,7 +182,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt update
-sudo apt install yarn
+sudo apt install --no-install-recommends yarn
 
 # Install bower & gulp
 echo "---Installing bower & gulp for dependency management & dev tools---"
